b74c0ddd1d79ef47de3332c315bc6b79a01bdac1
nnthach <nguyenngocthach2301@gmail.com>
2026-07-23 15:40:16 +0700

    chore: remove useless function

--- lib/stripe/index.ts
+++ lib/stripe/index.ts
@@ -4,25 +4,4 @@ const stripeClient = new Stripe(process.env.STRIPE_SECRET_KEY || "");
 
 export default stripeClient;
 
-interface CreatePaymentProp {
-  amount: number;
-  currency: string;
-}
 
-export const createPayment = async ({
-  amount,
-  currency,
-}: CreatePaymentProp) => {
-  const paymentIntent = await stripeClient.paymentIntents.create({
-    amount: amount,
-    currency: currency,
-    automatic_payment_methods: {
-      enabled: true,
-    },
-  });
-  return paymentIntent;
-};
-
-export const confirmPayment = async (paymentIntentId: string) => {
-  return await stripeClient.paymentIntents.confirm(paymentIntentId);
-};
